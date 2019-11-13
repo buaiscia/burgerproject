29dc52b087730b53009b893f4122b90aebf9ebf4
Alex Buaiscia (HCI) <alex.buaiscia@gmail.com>
2019-11-13 16:01:11 +0100

    add burger and ingredient components plus some css

---
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import classes from './burgerIngredient.module.css'
+
+const burgerIngredient = (props) => {
+    let ingredient = null;
+
+    switch(props.type) {
+        case('bread-bottom'): 
+            ingredient = <div className={}></div>
+    }
+};
+
+export default burgerIngredient;
